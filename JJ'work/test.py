7e601581d6c8d211d3cd007d1f3b9ffb4bceee42
Owen <61456951+Onyenwe327@users.noreply.github.com>
2025-02-07 09:22:32 -0800

    Update test.py

--- JJ'work/test.py
+++ JJ'work/test.py
@@ -3,5 +3,5 @@ import gymnasium as gym
 env = gym.make("Humanoid-v4", render_mode="human")
 env.reset()
 for _ in range(10000):
-    env.step(env.action_space.sample())  # 采取随机动作
+    env.step(env.action_space.sample())  
 env.close()
