f0ede3326f6f0c42c862bb26b063db4f8076023f
Jiajin Zhang <jiajinzh@a01-06.hpc.usc.edu>
2025-06-03 09:16:30 -0700

    updates

--- JJ/data/aggregated_results/rename.py
+++ JJ/data/aggregated_results/rename.py
@@ -1,15 +1,7 @@
 import os
 
-
-root_dir = './'  # 或者 '/path/to/your/folders/'
-
-for dirpath, dirnames, filenames in os.walk(root_dir):
-    for filename in filenames:
-        if ':' in filename:
-            old_path = os.path.join(dirpath, filename)
-            new_filename = filename.replace(':', '_')
-            new_path = os.path.join(dirpath, new_filename)
-            os.rename(old_path, new_path)
-            print(f"Renamed: {old_path} -> {new_path}")
-
-print("All done!")
+for folder in os.listdir('.'):
+    if os.path.isdir(folder):
+        new_name = folder.replace(':', '')
+        if new_name != folder:
+            os.rename(folder, new_name)
